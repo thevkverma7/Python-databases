a5b4a49f8a99832ecd23ca0757e209957f088cb0
Vivek Kumar Verma <thevkverma@gmail.com>
2022-06-02 06:40:33 +0530

    Add files via upload

---
+++ MySQL/MySQL Workbench's Results/Workbench.sql
@@ -0,0 +1,18 @@
+create database test1234344
+
+show databases
+
+drop database sudhanshu12345
+
+use sudhanshu12345
+
+select * from sudhanshu12345.mydata
+
+select * from sudhanshu12345.GlassData1;
+
+select * from sudhanshu12345.test;
+
+select * from sudhanshu12345.mydata;
+
+
+
